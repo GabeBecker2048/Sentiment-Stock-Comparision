056a8758c5be77b5392b6968858cc0e76bd1d7b9
Wyatt C <wyatthbc@gmail.com>
2023-12-03 23:03:22 -0600

    Changes to yfinance-test.py

---
+++ yfinance-test.py
@@ -0,0 +1,91 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+
+# Dictionary of stocks
+stocks_dict = {
+    "Apple": "AAPL",
+    "Microsoft": "MSFT",
+    "Alphabet (Google)": "GOOGL",
+    "Amazon": "AMZN",
+    "NVIDIA": "NVDA",
+    "Meta Platforms (Facebook)": "META",
+    "Berkshire Hathaway": "BRK.B",
+    "Tesla": "TSLA",
+    "Eli Lilly": "LLY",
+    "Visa": "V",
+    "Broadcom": "AVGO",
+    "UnitedHealth": "UNH",
+    "JPMorgan Chase": "JPM",
+    "Walmart": "WMT",
+    "Exxon Mobil": "XOM",
+    "Mastercard": "MA",
+    "Johnson & Johnson": "JNJ",
+    "Procter & Gamble": "PG",
+    "Oracle": "ORCL",
+    "Home Depot": "HD",
+    "Adobe": "ADBE",
+    "Chevron": "CVX",
+    "Costco": "COST",
+    "Merck": "MRK",
+    "Coca-Cola": "KO",
+    "AbbVie": "ABBV",
+    "Bank of America": "BAC",
+    "Pepsico": "PEP",
+    "Salesforce": "CRM",
+    "Netflix": "NFLX",
+    "McDonald": "MCD",
+    "AMD": "AMD",
+    "Cisco": "CSCO",
+    "Thermo Fisher Scientific": "TMO",
+    "Intel": "INTC",
+    "Abbott Laboratories": "ABT",
+    "T-Mobile US": "TMUS",
+    "Pfizer": "PFE",
+    "Comcast": "CMCSA",
+    "Walt Disney": "DIS",
+    "Nike": "NKE",
+    "Danaher": "DHR",
+    "Intuit": "INTU",
+    "Verizon": "VZ",
+    "Wells Fargo": "WFC",
+    "Philip Morris": "PM",
+    "QUALCOMM": "QCOM",
+    "Amgen": "AMGN",
+    "IBM": "IBM",
+    "Texas Instruments": "TXN"
+}
+
+# Get today's date and the date five days ago
+today = datetime.now()
+five_days_ago = today - timedelta(days=5)
+
+# Format dates for yfinance
+today_str = today.strftime('%Y-%m-%d')
+five_days_ago_str = five_days_ago.strftime('%Y-%m-%d')
+
+# Initialize lists for current and previous day prices
+current_prices = []
+previous_prices = []
+
+# Iterate over stocks
+for stock_name, stock_symbol in stocks_dict.items():
+    ticker = yf.Ticker(stock_symbol)
+
+    # Get historical market data for the last 5 days
+    hist = ticker.history(start=five_days_ago_str, end=today_str)
+
+    # Check if there are enough data points
+    if len(hist) < 2:
+        print(f"Not enough data for {stock_name}")
+        continue
+
+    # Get the last two days' prices
+    previous_price = hist['Close'].iloc[-2]
+    current_price = hist['Close'].iloc[-1]
+
+    # Append prices to lists
+    previous_prices.append((stock_name, previous_price))
+    current_prices.append((stock_name, current_price))
+
+print('Previous day prices:', previous_prices)
+print('Current prices:', current_prices)
