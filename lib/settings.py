9521935f61b4e67fce6dc2d654a3fc718096ed0c
GabeBecker2048 <78775796+GabeBecker2048@users.noreply.github.com>
2024-01-19 12:17:48 -0600

    Slightly changed CLI functionality

--- lib/settings.py
+++ lib/settings.py
@@ -44,5 +44,52 @@ class Settings:
         except json.JSONDecodeError:
             return
 
+    def settingsmod(self, settingsmod: str):
+        try:
+
+            # this splits settingsmod at the first '='
+            settingsmod_changed = False
+            for j, ichar in enumerate(settingsmod):
+                if ichar == '=':
+                    settingsmod = [settingsmod[:j], settingsmod[j + 1:]]
+                    settingsmod_changed = True
+                    break
+            # if no '=' was detected, the program raising an error
+            if not settingsmod_changed:
+                raise SettingsException
+
+            # if the user is attempting to change a setting that doesn't exist, raise an error
+            if settingsmod[0] not in self.s.keys():
+                raise SettingsException
+
+            # the search terms can only be edited in the settings.json file or the GUI, not with settingsmod
+            if settingsmod[0] == 'SearchTerms':
+                raise SettingsException
+
+            # Must account for settings that are not saved as strings
+            # Therefore, they must be converted into their correct data type
+            if settingsmod[0] in ['Running', 'NumArticles']:
+                # converts to bool
+                if settingsmod[0] == 'Running':
+                    if settingsmod[1].lower() == 'true':
+                        settingsmod[1] = True
+                    elif settingsmod[1].lower() == 'false':
+                        settingsmod[1] = False
+                    else:
+                        raise SettingsException
+
+                # converts to int
+                elif settingsmod[0] == 'NumArticles':
+                    settingsmod[1] = int(settingsmod[1])
+
+            # finally, we overwrite the current settings and save
+            self[settingsmod[0]] = settingsmod[1]
+            self.save()
+            return True
+
+        except SettingsException:
+            return False
+
+
 class SettingsException(Exception):
     pass
