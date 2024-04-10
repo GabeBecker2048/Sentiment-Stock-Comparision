2d65a4eb772e7ed50f394fecca739566475bd62b
gabe <gabebecker2048@gmail.com>
2024-04-09 20:46:20 -0500

    Utils Refactor
    Major utils refactor. Added some try-excepts and some extra print statements. Removed a some unnecessary code

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,7 @@
 import csv
 import subprocess
-from datetime import datetime, timedelta
+import warnings
+from datetime import datetime
 from gnews import GNews
 import yfinance as yf
 
@@ -24,63 +25,66 @@ def run_R_Script(r_script_path: str, RScriptLocation: str = "Rscript", outstream
 
 
 def generate_articles(settings: Settings, outstream=None):
-    print("Generating articles...\n", file=outstream)
 
-    # Configuration
-    news = GNews()
-    news.max_results = settings["NumArticles"]
+    try:
+        print("Generating articles...", file=outstream)
 
-    # before iterating through the search terms, we must first see what the maximum number of articles is:
-    max_terms = max(len(searchlist) for searchlist in settings["SearchTerms"].values())
-    max_articles = max_terms * settings["NumArticles"]
-    
-    # Searching and saving
-    csv_data = []
-    for searchlist in settings["SearchTerms"].values():
-        row = [searchlist[0]]
-        for search in searchlist:
+        # Configuration
+        news = GNews()
+        news.max_results = settings["NumArticles"]
 
-            print(f"\t\nGenerating news for the search {search}...\n", file=outstream)
-            inews = news.get_news(search)
+        # before iterating through the search terms, we must first see what the maximum number of articles is:
+        max_articles = max(len(searchlist) for searchlist in settings["SearchTerms"].values()) * settings["NumArticles"]
 
-            for article in inews:
-                title = article["title"].replace(',', '')
+        # Searching and saving
+        csv_data = []
+        for searchlist in settings["SearchTerms"].values():
+            row = [searchlist[0]]
+            for search in searchlist:
 
-                # if the article is already in the row, we continue to the next article
-                if title in row:
-                    continue
+                print(f"\n\tGenerating news for the search {search}...\n", file=outstream)
+                inews = news.get_news(search)
 
-                date_str = article["published date"].replace(',', '')
+                for article in inews:
+                    title = article["title"].replace(',', '')
 
-                # Format date to a more readable form
-                try:
-                    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-                    formatted_date = date_obj.strftime("%Y-%m-%d %H:%M:%S")
-                except ValueError:
-                    formatted_date = date_str
+                    # if the article is already in the row, we continue to the next article
+                    if title in row:
+                        continue
 
-                row += [title, formatted_date]
-                print(f"{title} - {formatted_date}, ", file=outstream)
-            print('\n', file=outstream)
+                    date_str = article["published date"].replace(',', '')
 
-        # Fill the remaining columns with empty strings if there are fewer than max_articles articles
-        row += [' '] * (1 + (max_articles * 2) - len(row))
+                    # Format date to a more readable form
+                    try:
+                        date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+                        formatted_date = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+                    except ValueError:
+                        formatted_date = date_str
 
-        csv_data.append(row)
-    
-    # Writing to CSV file
-    csv_header = ['Search Term']
-    for i in range(max_articles):
-        csv_header += [f"article {i + 1}", f"date {i + 1}"]
+                    row += [title, formatted_date]
+                    print(f"{title} - {formatted_date}, ", file=outstream)
 
-    newsfile = f"./lib/csv_data/news_data/news_{datetime.now().strftime('%Y-%m-%d')}.csv"
-    print(f"Saving articles to: {newsfile}", file=outstream)
-    with open(newsfile, "w", newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(csv_header)
-        csv_writer.writerows(csv_data)
+            # Fill the remaining columns with empty strings if there are fewer than max_articles articles
+            row += [' '] * (1 + (max_articles * 2) - len(row))
 
-    print("Articles Generated!\n", file=outstream)
+            csv_data.append(row)
+
+        # Writing to CSV file
+        csv_header = ['Search Term']
+        for i in range(max_articles):
+            csv_header += [f"article {i + 1}", f"date {i + 1}"]
+
+        newsfile = f"./lib/csv_data/news_data/news_{datetime.now().strftime('%Y-%m-%d')}.csv"
+        print(f"\nNews articles generated! Saving articles to: {newsfile}", file=outstream)
+        with open(newsfile, "w", newline='', encoding='utf-8') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(csv_header)
+            csv_writer.writerows(csv_data)
+
+        print("Articles saved!\n", file=outstream)
+
+    except Exception as e:
+        print(f"Error: {e}\n", file=outstream)
 
 
 def generate_sentiment_report(settings: Settings, outstream=None, gen_articles: bool = True):
@@ -90,7 +94,7 @@ def generate_sentiment_report(settings: Settings, outstream=None, gen_articles: 
         if gen_articles:
             generate_articles(settings, outstream)
 
-        print("\nGenerating Sentiment report...\n", file=outstream)
+        print("Generating Sentiment report...\n", file=outstream)
 
         # the two paths to the R scripts
         r_script_path1 = './lib/RScripts/sentiment_analysis.R'
@@ -99,75 +103,73 @@ def generate_sentiment_report(settings: Settings, outstream=None, gen_articles: 
         run_R_Script(r_script_path1, settings["RScriptLocation"], outstream)
         run_R_Script(r_script_path2, settings["RScriptLocation"], outstream)
 
-        print("R Scripts finished!\n", file=outstream)
+        print("Finished Sentiment Report!\n", file=outstream)
 
     except Exception as e:
         print(f"Error: {e}", file=outstream)
 
 
 def generate_stock_report(settings: Settings, outstream=None):
-    print("Generating Stock Report...\n", file=outstream)
 
-    # Get today's date and the date five days ago
-    today = datetime.now()
-    five_days_ago = today - timedelta(days=5)
+    try:
+        print("Generating Stock Report...\n", file=outstream)
 
-    # Format dates for yfinance
-    today_str = today.strftime('%Y-%m-%d')
-    five_days_ago_str = five_days_ago.strftime('%Y-%m-%d')
+        # Initialize lists for current and previous day prices
+        current_prices = []
+        previous_prices = []
+        differences = []
 
-    # Initialize lists for current and previous day prices
-    current_prices = []
-    previous_prices = []
-    differences = []
+        # Iterate over stocks
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+        for stock_symbol, stock_name in settings["SearchTerms"].items():
+            ticker = yf.Ticker(stock_symbol)
+            print(f"Getting data for {stock_symbol} - {stock_name[0]}...", file=outstream)
 
-    # Iterate over stocks
-    for stock_symbol, stock_name in settings["SearchTerms"].items():
-        ticker = yf.Ticker(stock_symbol)
+            # Get historical market data for the last 5 days
+            hist = ticker.history(period="5d")
 
-        # Get historical market data for the last 5 days
-        hist = ticker.history(start=five_days_ago_str, end=today_str)
+            # Check if there are enough data points
+            if len(hist) < 2:
+                print(f"Not enough data for {stock_name[0]}\n", file=outstream)
+                # Append NaN to list
+                previous_prices.append("NaN")
+                current_prices.append("NaN")
+                differences.append("NaN")
+                continue
 
-        # Check if there are enough data points
-        if len(hist) < 2:
-            print(f"Not enough data for {stock_name[0]}", file=outstream)
-            # Append zeros to lists
-            previous_prices.append("NaN")
-            current_prices.append("NaN")
-            differences.append("NaN")
-            continue
+            # Get the last two days' prices
+            previous_price = hist['Close'].iloc[-2]
+            current_price = hist['Close'].iloc[-1]
+            difference = current_price - previous_price
+            print(f"Daily difference: {difference}\n", file=outstream)
 
-        # Get the last two days' prices
-        previous_price = hist['Close'].iloc[-2]
-        current_price = hist['Close'].iloc[-1]
-        difference = current_price - previous_price
+            # Append prices to lists
+            previous_prices.append(previous_price)
+            current_prices.append(current_price)
+            differences.append(difference)
 
-        # Append prices to lists
-        previous_prices.append(previous_price)
-        current_prices.append(current_price)
-        differences.append(difference)
+        file_name = f"./lib/csv_data/stock_data/prices_{datetime.now().strftime('%Y-%m-%d')}.csv"
 
-    print('PREVIOUS DAY PRICES: ', previous_prices, file=outstream)
-    print('CURRENT PRICES: ', current_prices, file=outstream)
-    print('DIFFERENCES: ', differences, file=outstream)
+        print(f"Finished generating stock data! Saving to {file_name}\n", file=outstream)
+        with open(file_name, "w", newline='') as f:
+            w = csv.writer(f, delimiter=",", lineterminator='\r\n')
+            w.writerow(("Ticker", "Open", "Close", "Difference"))
+            for values in zip(settings["SearchTerms"], previous_prices, current_prices, differences):
+                w.writerow(values)
 
-    file_name = f"./lib/csv_data/stock_data/prices_{datetime.now().strftime('%Y-%m-%d')}.csv"
+        print(f'\nSuccessfully created stock data as {file_name}', file=outstream)
 
-    with open(file_name, "w", newline='') as f:
-        w = csv.writer(f, delimiter=",", lineterminator='\r\n')
-        w.writerow(("Ticker", "Open", "Close", "Difference"))
-        for values in zip(settings["SearchTerms"], previous_prices, current_prices, differences):
-            w.writerow(values)
+        print(f"Creating correlation analysis...", file=outstream)
+        rscript_path = "./lib/RScripts/stock_analysis.R"
+        run_R_Script(rscript_path, settings["RScriptLocation"], outstream)
 
-    print(f'\nSuccessfully created stock data as {file_name}', file=outstream)
-    
-    print(f"Creating stock analysis...", file=outstream)
-    rscript_path = "./lib/RScripts/stock_analysis.R"
-    run_R_Script(rscript_path, settings["RScriptLocation"], outstream)
+        print("Finished stock report!", file=outstream)
 
-    print("Finished stock report!", file=outstream)
+    except Exception as e:
+        print(f"Error: {e}", file=outstream)
 
 
 def run_all(settings: Settings, outstream=None):
     generate_sentiment_report(settings, outstream)
     generate_stock_report(settings, outstream)
+    print("Finished with daily report!", file=outstream)
